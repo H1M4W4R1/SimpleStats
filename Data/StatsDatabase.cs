3acfbba702934c0c1efc96f699b207103a5d729c
Patryk Pastuszka <nov11157@gmail.com>
2025-09-06 03:10:38 +0200

    Cleanup of Core/Storage folder structure.

--- Data/StatsDatabase.cs
+++ Data/StatsDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using JetBrains.Annotations;
 using Systems.SimpleCore.Storage;
+using Systems.SimpleCore.Storage.Databases;
 using Systems.SimpleStats.Data.Statistics;
 
 namespace Systems.SimpleStats.Data
